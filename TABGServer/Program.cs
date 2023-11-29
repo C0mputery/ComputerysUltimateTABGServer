215f39af86b4bcf87e4052d11e5c9fc6334fb3d9
Computery <77170025+C0mputery@users.noreply.github.com>
2023-11-28 19:30:44 -0500

    Cleaned Up main + program.cs

--- TABGServer/Program.cs
+++ TABGServer/Program.cs
@@ -5,58 +5,55 @@ namespace TABGCommunityServer
 {
     class TABGCommunityServer
     {
+        static ushort enetPort = 9701;
+        static ushort maxEnetClients = 256;
+        static Host? enetHost;
+        static Address enetAddress;
+        static Event enetEvent;
+
         static void Main()
         {
-            ushort port = 9701;
-            ushort maxClients = 256;
-            Console.WriteLine("TABG COMMUNITY SERVER v1 STARTED");
+            Console.WriteLine("TABG COMMUNITY SERVER v1 BOOTING");
 
             ENet.Library.Initialize();
-
-            using Host enetHost = new Host();
-
-            Address address = new Address();
-
-            address.Port = port;
-            address.SetIP("0.0.0.0");
-
-            enetHost.Create(address, maxClients);
-
-            Event netEvent;
+            enetHost = new Host();
+            enetAddress = new Address() { Port = enetPort };
+            enetAddress.SetIP("0.0.0.0");
+            enetHost.Create(enetAddress, maxEnetClients);
 
             Console.WriteLine("Server Started!");
 
             while (!Console.KeyAvailable)
             {
-                while (enetHost.CheckEvents(out netEvent) > 0 && !(enetHost.Service(15, out netEvent) <= 0))
+                while (enetHost.CheckEvents(out enetEvent) > 0 && !(enetHost.Service(15, out enetEvent) <= 0))
                 {
-                    switch (netEvent.Type)
+                    switch (enetEvent.Type)
                     {
                         case EventType.None:
                             break;
 
                         case EventType.Connect:
-                            Console.WriteLine("Client connected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                            Console.WriteLine("Client connected - ID: " + enetEvent.Peer.ID + ", IP: " + enetEvent.Peer.IP);
                             break;
 
                         case EventType.Disconnect:
-                            Console.WriteLine("Client disconnected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                            Console.WriteLine("Client disconnected - ID: " + enetEvent.Peer.ID + ", IP: " + enetEvent.Peer.IP);
                             break;
 
                         case EventType.Timeout:
-                            Console.WriteLine("Client timeout - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                            Console.WriteLine("Client timeout - ID: " + enetEvent.Peer.ID + ", IP: " + enetEvent.Peer.IP);
                             break;
                         case EventType.Receive:
-                            byte[] enetPacket = new byte[netEvent.Packet.Length];
-                            netEvent.Packet.CopyTo(enetPacket);
+                            byte[] enetPacket = new byte[enetEvent.Packet.Length];
+                            enetEvent.Packet.CopyTo(enetPacket);
 
                             EventCode code = (EventCode)enetPacket[0];
                             byte[] buffer = new byte[enetPacket.Length - 1];
                             Array.Copy(enetPacket, 1, buffer, 0, buffer.Length);
 
-                            PacketHandler.Handle(netEvent.Peer, code, buffer);
+                            PacketHandler.Handle(enetEvent.Peer, code, buffer);
 
-                            netEvent.Packet.Dispose();
+                            enetEvent.Packet.Dispose();
                             break;
                     }
                 }
