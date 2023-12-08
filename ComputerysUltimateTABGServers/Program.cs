4df4ecd166f0558fcba0530c66fc5d7a9959cf43
Computery <77170025+C0mputery@users.noreply.github.com>
2023-12-08 14:51:52 -0500

    some stuff

--- ComputerysUltimateTABGServers/Program.cs
+++ ComputerysUltimateTABGServers/Program.cs
@@ -4,6 +4,12 @@ namespace ComputerysUltimateTABGServers
 {
     class TABGCommunityServer
     {
+        static string cutsLogo = @"   ____   _   _   _____   ____      _        ___         ___  
+  / ___| | | | | |_   _| / ___|    / |      / _ \       / _ \ 
+ | |     | | | |   | |   \___ \    | |     | | | |     | | | |
+ | |___  | |_| |   | |    ___) |   | |  _  | |_| |  _  | |_| |
+  \____|  \___/    |_|   |____/    |_| (_)  \___/  (_)  \___/ ";
+
         static void Main()
         {
             StartUp();
@@ -13,11 +19,16 @@ namespace ComputerysUltimateTABGServers
 
         static void StartUp()
         {
-            Console.WriteLine("CUTS BOOTING, LETS FUCKIN GO");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(cutsLogo);
+            Console.ResetColor();
 
             ENet.Library.Initialize();
 
-            RoomManager.MakeRoom(4949, 100, "CUTS TEST SERVER");
+            RoomManager.MakeRoom(4949, 50, "TEST SERVER");
+#if UsingTabgServerList
+           TabgServerList.TabgServerListManager.StartServerListHeartbeat();
+#endif
         }
 
         static void MainLoop()
