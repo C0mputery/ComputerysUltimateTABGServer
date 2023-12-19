fb27b4bd6a6dbe8fd290c777aee20bd8c5662241
Computery <77170025+C0mputery@users.noreply.github.com>
2023-12-19 09:30:18 -0500

    Room changes

--- ComputerysUltimateTABGServers/Rooms/Room/Room.cs
+++ ComputerysUltimateTABGServers/Rooms/Room/Room.cs
@@ -4,7 +4,7 @@ using ComputerysUltimateTABGServer.Packets;
 
 namespace ComputerysUltimateTABGServer.Rooms
 {
-    public partial class Room
+    public partial class Room : IDisposable
     {
         public static readonly FrozenDictionary<EventCode, PacketHandlerDelegate> PacketHandlers = new Dictionary<EventCode, PacketHandlerDelegate>
         {
@@ -18,7 +18,9 @@ namespace ComputerysUltimateTABGServer.Rooms
         public Address m_EnetAddress;
         public Event m_EnetEvent;
         public int m_MaxClients;
+
         public bool m_ShouldEndRoom = false;
+        public Task? Task = null;
 
         public double m_DelayBetweenTicks = 16;
         public DateTime m_LastTickTime = DateTime.Now;
@@ -32,5 +34,13 @@ namespace ComputerysUltimateTABGServer.Rooms
             m_MaxClients = maxClients;
             m_DelayBetweenTicks = delayBetweenTicks;
         }
+
+        // TODO: Make this actually dispose of the room.
+        // Do not disspose of the room task, the garbage collector will do that for us.
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
     }
 }
