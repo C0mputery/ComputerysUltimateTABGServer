73f34b69dca898b84d2f07516118ca6a88d31980
Computery <77170025+C0mputery@users.noreply.github.com>
2023-12-21 11:26:19 -0500

    added range check

--- ComputerysUltimateTABGServers/Ticks/TickTypes/PlayerTick.cs
+++ ComputerysUltimateTABGServers/Ticks/TickTypes/PlayerTick.cs
@@ -1,5 +1,7 @@
 ﻿using ComputerysUltimateTABGServer.DataTypes.Player;
 using ComputerysUltimateTABGServer.Rooms;
+using System.Collections.Frozen;
+using System.Numerics;
 
 namespace ComputerysUltimateTABGServer.Ticks
 {
@@ -7,6 +9,15 @@ namespace ComputerysUltimateTABGServer.Ticks
     {
         public static void PlayerTick(Room room)
         {
+            foreach (Player player in room.m_Players.Values)
+            {
+                Player[] playersInRange = FindPlayersInRange(player, room);
+            }
+        }
+
+        public static Player[] FindPlayersInRange(Player player, Room room)
+        {
+            return room.m_Players.Values.Where(playerToCheckDistance => Vector3.Distance(playerToCheckDistance.m_Position, player.m_Position) <= room.m_PlayerUpdateRange).ToArray();
         }
     }
 }
