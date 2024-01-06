ff5ef0eb5ae2db6aaadafbf49c7f8f6152b821ae
Computery <77170025+C0mputery@users.noreply.github.com>
2024-01-05 21:05:53 -0500

    one commit a day

---
+++ Packets/PacketTypes/PlayerFirePacket.cs
@@ -0,0 +1,22 @@
+﻿using ComputerysUltimateTABGServer.Rooms;
+using ENet;
+
+namespace ComputerysUltimateTABGServer.Packets
+{
+    public static partial class PacketTypes
+    {
+        public static void PlayerFirePacket(Peer peer, byte[] receivedPacketRaw, Room room)
+        {
+            using (MemoryStream receivedPacketMemoryStream = new MemoryStream(receivedPacketRaw))
+            using (BinaryReader receivedPacketBinaryReader = new BinaryReader(receivedPacketMemoryStream))
+            {
+                byte playerIndex = receivedPacketBinaryReader.ReadByte();
+                FiringMode firingMode = (FiringMode)receivedPacketBinaryReader.ReadByte();
+                int ammoType = receivedPacketBinaryReader.ReadInt32();
+                if (receivedPacketRaw.Length - 6 > 0)
+                {
+                }
+            }
+        }
+    }
+}
